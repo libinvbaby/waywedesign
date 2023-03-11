ed61af8c32b11d951c153e20dcf33bd23ba48c03
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-03-11 17:19:41 +0530

    updted

--- src/components/ServiceSideMenu.js
+++ src/components/ServiceSideMenu.js
@@ -10,18 +10,18 @@ const ServiceSideMenu = () => {
           <div className="service-details__sidebar-service">
             <h4 className="service-details__sidebar-title">Categories</h4>
             <ul className="service-details__sidebar-service-list list-unstyled">
-              <li><Link to="">Software & Platform Development <span className="icon-right-arrow" /></Link>
+              <li><Link to="/software-development-platform">Software & Platform Development <span className="icon-right-arrow" /></Link>
               </li>
-              <li><Link to="">Digital Marketing <span className="icon-right-arrow" /></Link>
+              <li><Link to="/digital-marketing">Digital Marketing <span className="icon-right-arrow" /></Link>
               </li>
               <li><Link to="/cloud-service">Cloud Service<span className="icon-right-arrow" /></Link></li>
-              <li><Link to="">HRM <span className="icon-right-arrow" /></Link></li>
-              <li><Link to=""> Digital Academy<span className="icon-right-arrow" /></Link></li>
+              <li><Link to="/hrm">HRM <span className="icon-right-arrow" /></Link></li>
+              <li><Link to="/digital-marketing-academy"> Digital Academy<span className="icon-right-arrow" /></Link></li>
               <li><Link to=""> <span className="icon-right-arrow" /></Link></li>
             </ul>
           </div>
           <div className="service-details__need-help">
-            <div className="service-details__need-help-bg" style={{backgroundImage: 'url(assets/images/backgrounds/service-details-need-help-bg.jpg)'}}>
+            <div className="service-details__need-help-bg" style={{backgroundImage: 'url(assets/images/backgrounds/service-details-need-help-bg.webp)'}}>
             </div>
             <div className="service-details__need-help-bg-overly" />
             <div className="service-details__need-help-icon">
@@ -30,7 +30,7 @@ const ServiceSideMenu = () => {
             <h2 className="service-details__need-help-text-box">Get Professional Help to Solve <br /> IT
               Software Problems</h2>
             <div className="service-details__need-help-contact">
-              <a href="tel:926668880000">92 666 888 0000</a>
+              <a href="tel:+91 7994643673">+91 799 464 3673</a>
               <p>Call Anytime Our <br /> to Experts</p>
             </div>
           </div>
